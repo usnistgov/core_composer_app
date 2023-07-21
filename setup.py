0368fa3d576a0f650fe3eb1f459faa2edc8cd056
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-07-21 03:07:48 +0000

    chore: upgrade 2.5.0
    Change-Id: I743fba7fcc7780515e530de6bbfe9b7f8eb19e7d

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_composer_app",
-    version="2.4.0",
+    version="2.5.0",
     description="Template and type composer for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
