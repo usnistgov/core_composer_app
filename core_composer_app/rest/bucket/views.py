27a772d19a0a0020c6979317c2ea9b08577fcea8
Guillaume-sousa <guillaume.sousa@nist.gov>
2025-06-30 09:41:03 -0400

    feat: openapi schema doc
    Change-Id: Id210e2743a6d3662d67c7932b25257be51795208

--- core_composer_app/rest/bucket/views.py
+++ core_composer_app/rest/bucket/views.py
@@ -3,19 +3,19 @@
 
 from django.http import Http404
 from django.utils.decorators import method_decorator
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiExample,
+    OpenApiParameter,
+    OpenApiResponse,
+)
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from core_main_app.access_control.exceptions import AccessControlError
-from core_main_app.commons import exceptions
-from core_main_app.commons.exceptions import DoesNotExist
-from core_main_app.rest.template_version_manager.abstract_views import (
-    AbstractTemplateVersionManagerDetail,
-)
-from core_main_app.utils.decorators import api_staff_member_required
 from core_composer_app.components.bucket import api as bucket_api
 from core_composer_app.components.type_version_manager import (
     api as type_version_manager_api,
@@ -24,31 +24,68 @@ from core_composer_app.rest.bucket.serializers import (
     BucketSerializer,
     BucketsSerializer,
 )
+from core_main_app.access_control.exceptions import AccessControlError
+from core_main_app.commons import exceptions
+from core_main_app.commons.exceptions import DoesNotExist
+from core_main_app.rest.template_version_manager.abstract_views import (
+    AbstractTemplateVersionManagerDetail,
+)
+from core_main_app.utils.decorators import api_staff_member_required
 
 
+@extend_schema(
+    tags=["Bucket"],
+    description="List all buckets, or create a new one",
+)
 class BucketList(APIView):
-    """List all buckets, or create a new one."""
+    """List all buckets, or create a new one"""
 
     permission_classes = (IsAuthenticated,)
 
+    @extend_schema(
+        summary="Get all buckets",
+        description="Get all buckets",
+        parameters=[
+            OpenApiParameter(
+                name="label",
+                description="Filter by label",
+                required=False,
+                type=str,
+            ),
+        ],
+        responses={
+            200: BucketSerializer(many=True),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Get all buckets",
+                summary="Retrieve all buckets",
+                description="Retrieve a list of all available buckets",
+                value=None,
+                request_only=True,
+                response_only=False,
+            ),
+            OpenApiExample(
+                "Get buckets by label",
+                summary="Retrieve buckets by label",
+                description="Retrieve a list of buckets filtered by label",
+                value={"label": "label"},
+                request_only=True,
+                response_only=False,
+            ),
+        ],
+    )
     def get(self, request):
         """Get all buckets
-
         Url Parameters:
-
             label: label
-
         Examples:
-
             ../bucket/
             ../bucket?label=[label]
-
         Args:
-
             request: HTTP request
-
         Returns:
-
             - code: 200
               content: List of buckets
             - code: 500
@@ -57,12 +94,10 @@ class BucketList(APIView):
         try:
             # Get objects
             object_list = bucket_api.get_all()
-
             # Apply filters
             label = self.request.query_params.get("label", None)
             if label is not None:
                 object_list = object_list.filter(label=label)
-
             # Serialize object
             serializer = BucketSerializer(object_list, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -72,23 +107,39 @@ class BucketList(APIView):
                 content, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    @extend_schema(
+        summary="Create a bucket",
+        description="Create a new bucket",
+        request=BucketSerializer,
+        responses={
+            201: BucketSerializer,
+            400: OpenApiResponse(description="Validation error"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Create a bucket",
+                summary="Create a new bucket",
+                description="Create a new bucket with the provided label and types",
+                request_only=True,
+                value={
+                    "label": "label",
+                    "types": ["<type_version_manager_id>"],
+                },
+            ),
+        ],
+    )
     @method_decorator(api_staff_member_required())
     def post(self, request):
         """Create a bucket
-
         Parameters:
-
             {
-                "label": "label",
-                "types": ["<type_version_manager_id>"]
+              "label": "label",
+              "types": ["<type_version_manager_id>"]
             }
-
         Args:
-
             request: HTTP request
-
         Returns:
-
             - code: 201
               content: Created bucket
             - code: 400
@@ -99,12 +150,10 @@ class BucketList(APIView):
         try:
             # Build serializer
             bucket_serializer = BucketSerializer(data=request.data)
-
             # Validate data
             bucket_serializer.is_valid(raise_exception=True)
             # Save data
             bucket_serializer.save()
-
             # Return the serialized data
             return Response(
                 bucket_serializer.data, status=status.HTTP_201_CREATED
@@ -119,6 +168,10 @@ class BucketList(APIView):
             )
 
 
+@extend_schema(
+    tags=["Bucket"],
+    description="Retrieve, update or delete a bucket",
+)
 class BucketDetail(APIView):
     """Retrieve, update or delete a bucket"""
 
@@ -126,13 +179,9 @@ class BucketDetail(APIView):
 
     def get_object(self, pk):
         """Get bucket from db
-
         Args:
-
             pk: ObjectId
-
         Returns:
-
             Bucket
         """
         try:
@@ -140,16 +189,39 @@ class BucketDetail(APIView):
         except DoesNotExist:
             raise Http404
 
+    @extend_schema(
+        summary="Retrieve a bucket",
+        description="Retrieve a bucket by ID",
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="Bucket ID",
+            ),
+        ],
+        responses={
+            200: BucketSerializer,
+            404: OpenApiResponse(description="Object was not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Retrieve a bucket",
+                summary="Retrieve a bucket by ID",
+                description="Retrieve a bucket by providing its ID",
+                value=None,
+                request_only=False,
+                response_only=True,
+            ),
+        ],
+    )
     def get(self, request, pk):
         """Retrieve a bucket
-
         Args:
-
             request: HTTP request
             pk: ObjectId
-
         Returns:
-
             - code: 200
               content: Bucket
             - code: 404
@@ -160,10 +232,8 @@ class BucketDetail(APIView):
         try:
             # Get object
             bucket = self.get_object(pk)
-
             # Serialize object
             serializer = BucketSerializer(bucket)
-
             # Return response
             return Response(serializer.data)
         except Http404:
@@ -175,17 +245,40 @@ class BucketDetail(APIView):
                 content, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    @extend_schema(
+        summary="Delete a bucket",
+        description="Delete a bucket by ID",
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="Bucket ID",
+            ),
+        ],
+        responses={
+            204: None,
+            404: OpenApiResponse(description="Object was not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Delete a bucket",
+                summary="Delete a bucket by ID",
+                description="Delete a bucket by providing its ID",
+                value=None,
+                request_only=True,
+                response_only=False,
+            ),
+        ],
+    )
     @method_decorator(api_staff_member_required())
     def delete(self, request, pk):
         """Delete a bucket
-
         Args:
-
             request: HTTP request
             pk: ObjectId
-
         Returns:
-
             - code: 204
               content: Deletion succeed
             - code: 404
@@ -196,10 +289,8 @@ class BucketDetail(APIView):
         try:
             # Get object
             bucket = self.get_object(pk)
-
             # delete object
             bucket_api.delete(bucket)
-
             # Return response
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Http404:
@@ -211,23 +302,45 @@ class BucketDetail(APIView):
                 content, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    @extend_schema(
+        summary="Update a bucket",
+        description="Update a bucket by ID",
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="Bucket ID",
+            ),
+        ],
+        request=BucketSerializer(partial=True),
+        responses={
+            200: BucketSerializer,
+            400: OpenApiResponse(description="Validation error"),
+            404: OpenApiResponse(description="Object was not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Update a bucket",
+                summary="Update a bucket by ID",
+                description="Update a bucket by providing its ID and the updated data",
+                request_only=True,
+                value={"label": "<label>"},
+            ),
+        ],
+    )
     @method_decorator(api_staff_member_required())
     def patch(self, request, pk):
         """Update bucket
-
         Parameters:
-
             {
-                "label": "<label>"
+              "label": "<label>"
             }
-
         Args:
-
             request: HTTP request
             pk: ObjectId
-
         Returns:
-
             - code: 200
               content: Updated bucket
             - code: 400
@@ -240,17 +353,14 @@ class BucketDetail(APIView):
         try:
             # Get object
             bucket = self.get_object(pk)
-
             # Build serializer
             bucket_serializer = BucketSerializer(
                 instance=bucket, data=request.data, partial=True
             )
-
             # Validate data
             bucket_serializer.is_valid(raise_exception=True)
             # Save data
             bucket_serializer.save(user=request.user)
-
             return Response(bucket_serializer.data, status=status.HTTP_200_OK)
         except ValidationError as validation_exception:
             content = {"message": validation_exception.detail}
@@ -265,18 +375,18 @@ class BucketDetail(APIView):
             )
 
 
+@extend_schema(
+    tags=["Type Version Manager"],
+    description="Set new list of buckets for a type version manager",
+)
 class TypeVersionManagerBuckets(AbstractTemplateVersionManagerDetail):
     """Set new list of buckets for a type version manager"""
 
     def get_object(self, pk):
         """Get template version manager from db
-
         Args:
-
             pk: ObjectId
-
         Returns:
-
             TemplateVersionManager
         """
         try:
@@ -287,28 +397,54 @@ class TypeVersionManagerBuckets(AbstractTemplateVersionManagerDetail):
         except exceptions.DoesNotExist:
             raise Http404
 
+    @extend_schema(
+        summary="Set new list of buckets for a type version manager",
+        description="Update the list of buckets associated with a type version manager",
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="Type Version Manager ID",
+            ),
+        ],
+        request=BucketsSerializer(many=True),
+        responses={
+            200: None,
+            400: OpenApiResponse(description="Validation error"),
+            403: OpenApiResponse(description="Access Forbidden"),
+            404: OpenApiResponse(description="Object was not found"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Set new list of buckets",
+                summary="Update the list of buckets",
+                description="Update the list of buckets associated with a type version manager",
+                request_only=True,
+                value=[
+                    {"id": "<bucket_id>"},
+                    {"id": "<bucket_id>"},
+                ],
+            ),
+        ],
+    )
     @method_decorator(api_staff_member_required())
     def patch(self, request, pk):
         """Set new list of buckets for a type version manager
-
         Parameters:
-
             [
-                {
-                    "id":"<bucket_id>"
-                },
-                {
-                    "id":"<bucket_id>"
-                }
+              {
+                "id":"<bucket_id>"
+              },
+              {
+                "id":"<bucket_id>"
+              }
             ]
-
         Args:
-
             request: HTTP request
             pk: ObjectId
-
         Returns:
-
             - code: 200
               content: None
             - code: 400
@@ -323,21 +459,16 @@ class TypeVersionManagerBuckets(AbstractTemplateVersionManagerDetail):
         try:
             # Get object
             type_version_manager = self.get_object(pk)
-
             # Serialize data
             serializer = BucketsSerializer(data=request.data, many=True)
-
             # Validate data
             serializer.is_valid(raise_exception=True)
-
             # Get list of unique ids
             bucket_ids = set(
                 [bucket["id"] for bucket in serializer.validated_data]
             )
-
             # act
             bucket_api.update_type_buckets(type_version_manager, bucket_ids)
-
             return Response(status=status.HTTP_200_OK)
         except Http404:
             content = {"message": "Object not found."}
--- core_composer_app/rest/type_version_manager/views.py
+++ core_composer_app/rest/type_version_manager/views.py
@@ -1,29 +1,68 @@
 """ REST views for the type version manager API
 """
 
+from django.db.migrations.serializer import TypeSerializer
 from django.utils.decorators import method_decorator
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (
+    OpenApiParameter,
+    extend_schema,
+    OpenApiExample,
+    OpenApiResponse,
+)
 from rest_framework.permissions import IsAuthenticated
 
-from core_main_app.rest.template_version_manager.views import (
-    AbstractTemplateVersionManagerList,
-)
-from core_main_app.utils.decorators import api_staff_member_required
 from core_composer_app.components.type_version_manager import (
     api as type_version_manager_api,
 )
 from core_composer_app.rest.type_version_manager.abstract_views import (
     AbstractTypeList,
 )
+from core_composer_app.rest.type_version_manager.serializers import (
+    TypeVersionManagerSerializer,
+    CreateTypeSerializer,
+)
+from core_main_app.rest.template_version_manager.views import (
+    AbstractTemplateVersionManagerList,
+)
+from core_main_app.utils.decorators import api_staff_member_required
 
 
+@extend_schema(
+    tags=["Type Version Manager"],
+    description="List all global type version managers",
+)
 class GlobalTypeVersionManagerList(AbstractTemplateVersionManagerList):
     """List all global type version managers"""
 
+    @extend_schema(
+        summary="Get global type version managers",
+        description="Retrieve a list of all global type version managers",
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+                description="Filter by title",
+            ),
+            OpenApiParameter(
+                name="is_disabled",
+                type=OpenApiTypes.BOOL,
+                location=OpenApiParameter.QUERY,
+                description="Filter by is_disabled",
+            ),
+        ],
+        responses={
+            200: TypeVersionManagerSerializer(many=True),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
+    def get(self, request):
+        return super().get(request)
+
     def get_template_version_managers(self):
         """Get global type version managers
-
         Returns:
-
             TypeVersionManager
         """
         return type_version_manager_api.get_global_version_managers(
@@ -31,16 +70,44 @@ class GlobalTypeVersionManagerList(AbstractTemplateVersionManagerList):
         )
 
 
+@extend_schema(
+    tags=["Type Version Manager"],
+    description="List all user type version managers",
+)
 class UserTypeVersionManagerList(AbstractTemplateVersionManagerList):
     """List all user type version managers"""
 
     permission_classes = (IsAuthenticated,)
 
+    @extend_schema(
+        summary="Get user type version managers",
+        description="Retrieve a list of all type version managers for the current user",
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+                description="Filter by title",
+            ),
+            OpenApiParameter(
+                name="is_disabled",
+                type=OpenApiTypes.BOOL,
+                location=OpenApiParameter.QUERY,
+                description="Filter by is_disabled",
+            ),
+        ],
+        responses={
+            200: TypeVersionManagerSerializer(many=True),
+            403: OpenApiResponse(description="Access Forbidden"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
+    def get(self, request):
+        return super().get(request)
+
     def get_template_version_managers(self):
         """Get all user type version managers
-
         Returns:
-
             TypeVersionManager
         """
         return type_version_manager_api.get_version_managers_by_user(
@@ -48,34 +115,51 @@ class UserTypeVersionManagerList(AbstractTemplateVersionManagerList):
         )
 
 
+@extend_schema(
+    tags=["Type"],
+    description="Create a user type",
+)
 class UserTypeList(AbstractTypeList):
     """Create a user type"""
 
     permission_classes = (IsAuthenticated,)
 
+    @extend_schema(
+        summary="Create a user type & type version manager",
+        description="Create a new user type and its associated type version manager",
+        request=CreateTypeSerializer,
+        responses={
+            201: TypeSerializer,
+            400: OpenApiResponse(description="Validation Error / Bad Request"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Create a user type",
+                summary="Create a new user type",
+                description="Create a new user type and its associated type version manager",
+                request_only=True,
+                value={
+                    "title": "title",
+                    "filename": "filename",
+                    "content": "<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'><xs:simpleType name='root'/></xs:schema>",
+                },
+            ),
+        ],
+    )
     def post(self, request):
         """Create a user type & type version manager
-
         Parameters:
-
             {
-                "title": "title",
-                "filename": "filename",
-                "content": "<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'>
-                            <xs:simpleType name='root'/></xs:schema>"
-
+              "title": "title",
+              "filename": "filename",
+              "content": "<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'> <xs:simpleType name='root'/></xs:schema>"
             }
-
         Note:
-
             "dependencies_dict": json.dumps({"schemaLocation1": "id1" ,"schemaLocation2":"id2"})
-
         Args:
-
             request: HTTP request
-
         Returns:
-
             - code: 201
               content: Type
             - code: 400
@@ -90,32 +174,50 @@ class UserTypeList(AbstractTypeList):
         return str(self.request.user.id)
 
 
+@extend_schema(
+    tags=["Type"],
+    description="Create a global type",
+)
 class GlobalTypeList(AbstractTypeList):
     """Create a global type"""
 
+    @extend_schema(
+        summary="Create a global type & type version manager",
+        description="Create a new global type and its associated type version manager",
+        request=CreateTypeSerializer,
+        responses={
+            201: TypeSerializer,
+            400: OpenApiResponse(description="Validation Error / Bad Request"),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+        examples=[
+            OpenApiExample(
+                "Create a global type",
+                summary="Create a new global type",
+                description="Create a new global type and its associated type version manager",
+                request_only=True,
+                value={
+                    "title": "title",
+                    "filename": "filename",
+                    "content": "<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'><xs:simpleType name='root'/></xs:schema>",
+                },
+            ),
+        ],
+    )
     @method_decorator(api_staff_member_required())
     def post(self, request):
         """Create a global type & type version manager
-
         Parameters:
-
             {
-                "title": "title",
-                "filename": "filename",
-                "content": "<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'>
-                            <xs:simpleType name='root'/></xs:schema>"
+              "title": "title",
+              "filename": "filename",
+              "content": "<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema'> <xs:simpleType name='root'/></xs:schema>"
             }
-
         Note:
-
             "dependencies_dict": json.dumps({"schemaLocation1": "id1" ,"schemaLocation2":"id2"})
-
         Args:
-
             request: HTTP request
-
         Returns:
-
             - code: 201
               content: Type
             - code: 400
